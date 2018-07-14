e6598eae40c658f2d2b6bf3f32c45d7caa620e8c
retgits <lstigter@gmail.com>
2018-07-14 14:19:21 -0700

    Update imports.go with new activities and triggers

--- flogoweb/imports.go
+++ flogoweb/imports.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	// Default Flogo activities
+	// Flogo action
 	_ "github.com/TIBCOSoftware/flogo-contrib/action/flow"
+
+	// Activities from https://github.com/TIBCOSoftware/flogo-contrib
 	_ "github.com/TIBCOSoftware/flogo-contrib/activity/actreply"
 	_ "github.com/TIBCOSoftware/flogo-contrib/activity/actreturn"
 	_ "github.com/TIBCOSoftware/flogo-contrib/activity/aggregate"
@@ -14,14 +16,18 @@ import (
 	_ "github.com/TIBCOSoftware/flogo-contrib/activity/counter"
 	_ "github.com/TIBCOSoftware/flogo-contrib/activity/error"
 	_ "github.com/TIBCOSoftware/flogo-contrib/activity/gpio"
+	_ "github.com/TIBCOSoftware/flogo-contrib/activity/inference"
 	_ "github.com/TIBCOSoftware/flogo-contrib/activity/kafkapub"
 	_ "github.com/TIBCOSoftware/flogo-contrib/activity/lambda"
 	_ "github.com/TIBCOSoftware/flogo-contrib/activity/log"
 	_ "github.com/TIBCOSoftware/flogo-contrib/activity/mapper"
+	_ "github.com/TIBCOSoftware/flogo-contrib/activity/mongodb"
 	_ "github.com/TIBCOSoftware/flogo-contrib/activity/rest"
 	_ "github.com/TIBCOSoftware/flogo-contrib/activity/subflow"
 	_ "github.com/TIBCOSoftware/flogo-contrib/activity/twilio"
 	_ "github.com/TIBCOSoftware/flogo-contrib/activity/wsmessage"
+
+	// Triggers from https://github.com/TIBCOSoftware/flogo-contrib
 	_ "github.com/TIBCOSoftware/flogo-contrib/trigger/cli"
 	_ "github.com/TIBCOSoftware/flogo-contrib/trigger/coap"
 	_ "github.com/TIBCOSoftware/flogo-contrib/trigger/kafkasub"
@@ -29,21 +35,28 @@ import (
 	_ "github.com/TIBCOSoftware/flogo-contrib/trigger/mqtt"
 	_ "github.com/TIBCOSoftware/flogo-contrib/trigger/rest"
 	_ "github.com/TIBCOSoftware/flogo-contrib/trigger/timer"
-	// Activities from retgits/flogo-components
+
+	// Activities from https://github.com/retgits/flogo-components
 	_ "github.com/retgits/flogo-components/activity/addtodate"
 	_ "github.com/retgits/flogo-components/activity/amazons3"
 	_ "github.com/retgits/flogo-components/activity/amazonsqssend"
-	_ "github.com/retgits/flogo-components/activity/amazonssm"
-        _ "github.com/retgits/flogo-components/activity/commandparser"
-        _ "github.com/retgits/flogo-components/activity/downloadfile"
+	_ "github.com/retgits/flogo-components/activity/awsssm"
+	_ "github.com/retgits/flogo-components/activity/commandparser"
+	_ "github.com/retgits/flogo-components/activity/downloadfile"
 	_ "github.com/retgits/flogo-components/activity/dynamodbinsert"
 	_ "github.com/retgits/flogo-components/activity/dynamodbquery"
 	_ "github.com/retgits/flogo-components/activity/githubissues"
 	_ "github.com/retgits/flogo-components/activity/gzip"
-        _ "github.com/retgits/flogo-components/activity/iftttwebhook"
+	_ "github.com/retgits/flogo-components/activity/iftttwebhook"
+	_ "github.com/retgits/flogo-components/activity/null"
+	_ "github.com/retgits/flogo-components/activity/pubnubpublisher"
+	_ "github.com/retgits/flogo-components/activity/queryparser"
 	_ "github.com/retgits/flogo-components/activity/randomnumber"
 	_ "github.com/retgits/flogo-components/activity/randomstring"
-        _ "github.com/retgits/flogo-components/activity/tomlreader"
+	_ "github.com/retgits/flogo-components/activity/tomlreader"
 	_ "github.com/retgits/flogo-components/activity/trellocard"
-        _ "github.com/retgits/flogo-components/activity/writetofile"
+	_ "github.com/retgits/flogo-components/activity/writetofile"
+
+	// Triggers from https://github.com/retgits/flogo-components
+	_ "github.com/retgits/flogo-components/trigger/pubnubsubscriber"
 )
