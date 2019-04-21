d5fffc5ad96b982575859f47421744b78515417f
retgits <lstigter@gmail.com>
2019-04-21 13:16:08 -0700

    feat: add update script for jfrog-cli

---
+++ jfrog-cli/update.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Description: Update JFrog CLI images
+# Author: retgits <https://github.com/retgits>
+# Last Updated: 2019-04-21
+
+# Set the current and new version
+CURR=1.25.0
+NEW=$1
+
+# Loop over the images and build new ones
+for d in */ ; do
+    cd $d
+    sed -i "s/$CURR/$1/g" Dockerfile
+    a=`cat Dockerfile | sed -n '2 p'`
+    b=${a:2}
+    $b
+    cd ..
+done
+
+# Push new containers to DockerHub
+docker push retgits/jfrog-cli:$NEW
+docker push retgits/jfrog-cli:$NEW-alpine
+
+# Replace CURR with NEW in this file
+sed -i "s/CURR=$CURR/CURR=$NEW/g" update.sh
